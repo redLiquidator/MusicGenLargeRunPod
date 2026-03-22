8fd8560d2ba8b1f3be43cb7cd4e9e4d8a4deaf54
lee jin joo <inufuni@naver.com>
2026-03-22 23:47:01 +0900

    Journal.java

---
+++ Journal.java
@@ -0,0 +1,4 @@
+**Mar 22**
+Any way to solve MusicGenLarge's lack of melody issue?
+Any way to create melody first then let the bossa jazz background music play according to the melody?  
+Let's get to know about Music Gen Melody. 
